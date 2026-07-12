9cbb6fdb566b6e6ebc0416718d58b18f138b4cc6
David Martin <irldavem@gmail.com>
2026-07-12 23:23:02 +0100

    ending: decompile four more credits-module helper functions
    Signed-off-by: David Martin <irldavem@gmail.com>

--- src/ending/ending.c
+++ src/ending/ending.c
@@ -4,11 +4,35 @@
 #include <libgpu.h>
 #include <libgte.h>
 
-extern s32 D_800A6528;
+extern s16* D_800A6528;
 extern s32 D_800AF410;
+
+typedef struct {
+    u8 pad0[8];
+    s32 unk8;
+} D_80075D00_t;
+extern D_80075D00_t* D_80075D00;
 
 void func_80033B70();
 void func_80036298();
+
+typedef struct EndingObj {
+    /* 0x00 */ void* unk0;
+    /* 0x04 */ void* unk4;
+    /* 0x08 */ u8 pad08[0x28 - 0x08];
+    /* 0x28 */ s32 unk28;
+    /* 0x2C */ s32 unk2C;
+    /* 0x30 */ s32 unk30;
+    /* 0x34 */ s32 unk34;
+    /* 0x38 */ s16 unk38;
+    /* 0x3A */ s16 unk3A;
+    /* 0x3C */ s16 unk3C;
+    /* 0x3E */ s16 unk3E;
+    /* 0x40 */ s16 unk40;
+    /* 0x42 */ s16 unk42;
+    /* 0x44 */ s16 unk44;
+    /* 0x46 */ s16 unk46;
+} EndingObj;
 
 INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A0030);
 
@@ -42,7 +66,7 @@ INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A19A4);
 
 INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A1E20);
 
-static void func_800A1ED4(s32 arg0) { D_800A6528 = arg0; }
+static void func_800A1ED4(s32 arg0) { D_800A6528 = (s16*)arg0; }
 
 s32 func_800A1EE4(void) { return 0; }
 
@@ -68,7 +92,10 @@ INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A21CC);
 
 INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A2248);
 
-INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A2274);
+s32 func_800A2274(void) {
+    D_800A6528 = (s16*)((u8*)D_800A6528 - (*D_800A6528 + 1) * 2);
+    return 1;
+}
 
 INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A22A4);
 
@@ -85,7 +112,13 @@ INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A2380);
 
 INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A23F8);
 
-INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A2420);
+s32 func_800A2420(void) {
+    s16* temp_v1;
+
+    temp_v1 = D_800A6528;
+    D_800A6528 = temp_v1 + 1;
+    return (u32)D_80075D00->unk8 >= (u32)*temp_v1;
+}
 
 /* resets callbacks, graphics, pad, and geometry state */
 static void func_800A2458(void) {
@@ -98,7 +131,22 @@ static void func_800A2458(void) {
     func_80033B70();
 }
 
-INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A24A8);
+void func_800A24A8(void) {
+    s32 temp_v0;
+    s32 new_var;
+
+    new_var = -1;
+loop_1:
+    temp_v0 = func_800484A8();
+    if (temp_v0 == new_var) {
+        VSync(0);
+        goto loop_1;
+    }
+    if (temp_v0 != 0) {
+        do {
+        } while (func_80048540(1) != 0);
+    }
+}
 
 INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A2504);
 
@@ -106,7 +154,22 @@ INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A273C);
 
 INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A2888);
 
-INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A2934);
+void func_800A2934(EndingObj* arg0, EndingObj* arg1) {
+    arg0->unk4 = arg1;
+    arg1->unk0 = arg0;
+    arg1->unk34 = 0x1000;
+    arg1->unk30 = 0x1000;
+    arg1->unk2C = 0x1000;
+    arg1->unk28 = 0x1000;
+    arg1->unk46 = 0;
+    arg1->unk44 = 0;
+    arg1->unk42 = 0;
+    arg1->unk40 = 0;
+    arg1->unk3E = 0;
+    arg1->unk3C = 0;
+    arg1->unk3A = 0;
+    arg1->unk38 = 0;
+}
 
 INCLUDE_ASM("asm/us/ending/nonmatchings/ending", func_800A2974);
 
